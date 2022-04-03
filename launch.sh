e7e8723c70e3ea6f3376d010a3241742120831ac
Lance Zhao <yilong877@gmail.com>
2022-04-03 15:08:14 +0800

    configures

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 monitor_counter=`xrandr -q | grep -w 'connected' | wc -l`
-if [ ${monitor_counter} -ne 1 ]; then
+if [ $monitor_counter -ne 1 ]; then
     MONITOR=DP-0 polybar mainbar --config=$HOME/.config/i3/polybar/config &
     MONITOR=HDMI-0 polybar secondbar --config=$HOME/.config/i3/polybar/config &
 else
