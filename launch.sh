4049af1ce4b93302bb4bae5c6f8bdd9390b651d6
Lance Zhao <yilong877@gmail.com>
2023-02-02 20:22:54 +0800

    update launch.sh

--- launch.sh
+++ launch.sh
@@ -2,9 +2,9 @@
 killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 monitor_counter=`xrandr -q | grep -w 'connected' | wc -l`
-if [ $monitor_counter -ne 1 ]; then
+if [ $monitor_counter -eq 1 ]; then
     MONITOR=DP-0 polybar mainbar --config=$HOME/.config/i3/polybar/config &
-    MONITOR=HDMI-0 polybar secondbar --config=$HOME/.config/i3/polybar/config &
 else
     MONITOR=DP-0 polybar mainbar --config=$HOME/.config/i3/polybar/config &
+    MONITOR=HDMI-0 polybar secondbar --config=$HOME/.config/i3/polybar/config &
 fi
